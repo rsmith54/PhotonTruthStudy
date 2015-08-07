e7a5ceafd80434fadd87ecba462d04054c8d4fa5
Russell Smith <rsmith@cern.ch>
2015-08-07 07:55:07 -0400

    changes to share

--- share/StudyPhotonTruth.Sherpa211.py
+++ share/StudyPhotonTruth.Sherpa211.py
@@ -5,7 +5,8 @@ from AthenaCommon import CfgMgr
 
 from glob import glob
 
-filelist = ['/r03/atlas/khoo/Data_2014/DAOD_TRUTH/user.khoo.361047.Sherpa_CT10_SinglePhotonPt140_280_BFilter.DAOD_TRUTH.e3587.030415.v3_EXT0.23956901/user.khoo.5246417.EXT0._000005.DAOD_TRUTH1.test.pool.root']
+#filelist = ['/r03/atlas/khoo/Data_2014/DAOD_TRUTH/user.khoo.361047.Sherpa_CT10_SinglePhotonPt140_280_BFilter.DAOD_TRUTH.e3587.030415.v3_EXT0.23956901/user.khoo.5246417.EXT0._000005.DAOD_TRUTH1.test.pool.root']
+filelist = ['/data/users/rsmith/mc15_13TeV.361047.Sherpa_CT10_SinglePhotonPt140_280_BFilter.merge.DAOD_TRUTH1.e3587_p2375/DAOD_TRUTH1.05969087._000001.pool.root.1']
 #filelist = ['/r03/atlas/khoo/Data_2014/DAOD_TRUTH/mc12_13TeV.177583.Sherpa_CT10_SinglePhotonMassiveCBPt140_280_BFilter.merge.DAOD_TRUTH1.e3083_p2321_tid05193453_00/DAOD_TRUTH1.05193453._000008.pool.root.1']
 
 ServiceMgr.EventSelector.InputCollections = filelist
@@ -29,7 +30,10 @@ topSequence = AlgSequence()
 topSequence += CfgMgr.PhotonTruthAlg("Gamma_140_280",
                                      RootStreamName=streamName,
                                      RootDirName='/GammaPt140',
-                                     PhotonRef_PtMin= 140e3,PhotonRef_PtMax= 280e3)
+                                     PhotonRef_PtMin= 140e3,
+                                     PhotonRef_PtMax= 280e3,
+                                     JetRJCut       = 5
+                                     )
 topSequence += CfgMgr.PhotonTruthAlg("Gamma_280_500",
                                      RootStreamName=streamName,
                                      RootDirName='/GammaPt280',
@@ -52,6 +56,7 @@ topSequence += CfgMgr.PhotonTruthAlg("Gamma_4000_inf",
                                      PhotonRef_PtMin=4000e3,PhotonRef_PtMax=1e9)
 
 #ServiceMgr.MessageSvc.defaultLimit = 9999999
+#theApp.EvtMax = 100
 theApp.EvtMax = -1
 ServiceMgr.EventSelector.SkipEvents = 0
 
