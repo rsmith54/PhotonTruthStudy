140ff15c2c8fb788de8cb9421f0d889b0cdffb75
Russell Smith <rsmith@cern.ch>
2015-08-09 16:23:36 -0400

    submission scripts

---
+++ share/StudyAllPhotonSherpa211.py
@@ -0,0 +1,28 @@
+from StudyPhotonTruthSherpa211 import *
+
+
+import os
+
+from optparse import OptionParser
+
+# print "parsing"
+# parser = OptionParser()
+# parser.add_option("--file"   , help="txt file listing all of your datasets" , default="")
+# (options, args) = parser.parse_args()
+
+
+print "input file"
+#inputFile = open(directoryListPhotonStudy.txt, 'r')
+inputFile = open('directoryListPhotonStudy.txt', 'r')
+
+for line in inputFile :
+    dirname = line.rstrip()
+
+    print os.listdir(dirname)
+    rawFileList = os.listdir(dirname)
+    fileList = []
+    for ifile in rawFileList :
+        fileList.append(dirname + '/' + ifile)
+
+    print fileList
+    studyPhotonTruth(fileList)
